5fdbd25eb2a5e02f74c850c9b73eee1dfca6b094
Derek O'Neill <derekoneil@gmail.com>
2017-12-05 11:46:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 var http = require('http');
 var express = require('express');
+var request = require('request');
 var path = require('path');
 var PORT = process.env.PORT || 8085;
 var app = express();
@@ -11,7 +12,15 @@ app.use(express.static('./public'));
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/public/alpha.html'));
 });
-
+app.get('/products', function (req, res) {
+    
+      request('http://alpha-office-rest-service:3000/products', function (error, response, body) {
+        console.log('error:', error); 
+        console.log('statusCode:', response && response.statusCode);
+        console.log('body:', body);
+          res.end(body);
+        });
+});
 app.listen(PORT, function () {
     console.log('listening on port ' + PORT)
 });
